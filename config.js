777e5f337ecee03d1e40403e1860925e9c5a968a
William Pradier <william.pradier2@gmail.com>
2024-02-08 08:37:51 +0100

    added s3 configuration

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
   s3: {
-    accessKeyId: '', // TODO
-    secretAccessKey: '', // TODO
-    endpoint: '', // TODO
-    bucket: '', // TODO
+    accessKeyId: process.env.ACCESS_KEY,
+    secretAccessKey: process.env.SECRET_KEY,
+    endpoint: 's3.eu-west-1.amazonaws.com',
+    bucket: 'esgi-cloud-exam-bucket-67522',
   },
   suffix: {
     small: '_small.jpg',
